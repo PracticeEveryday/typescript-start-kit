707915593b0d4c6c09f27f4fa396e7622224ed8d
PracticeEveryday <developerforus@gmail.com>
2023-11-30 17:43:37 +0900

    feat: 의존성 수정

--- config/index.ts
+++ config/index.ts
@@ -6,16 +6,6 @@ if (envFound.error) {
   throw new Error('.env 파일이 없습니다!!');
 }
 
-// const MONGO_URL = process.env["MONGO_URL"];
-// if (!MONGO_URL) {
-//   throw new Error(".env에 MONGO URL이 없습니다.");
-// }
-
-// const JWT_KEY = process.env["JWT_KEY"]
-// if (!JWT_KEY) {
-//   throw new Error(".env에 JWT_KEY가 없습니다.");
-// }
-
 export default {
   // 백엔드 포트번호
   PORT: process.env['PORT'] || 5000,
